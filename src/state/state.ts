e382d92f032595d82a0242294873ccb3ad98f99d
Cristopher Namchee <cristophernamchee12@gmail.com>
2019-12-14 14:26:47 +0700

    feat: Added more documentation (15%)

--- src/state/state.ts
+++ src/state/state.ts
@@ -25,8 +25,25 @@ export interface State {
  * It uses provider and user id to map the user's state
  */
 export class UserState {
+  /**
+   * A map which stores users state
+   *
+   * @private
+   * @static
+   * @memberof UserState
+   */
   private static readonly userState = new Map<string, State>();
 
+  /**
+   * Create new state for a user
+   *
+   * @param {string} provider Provider information
+   * @param {string} id User's ID
+   * @param {string} service Service's identifier
+   * @param {number} state Service's state
+   * @param {string} text Accumulated text
+   * @param {Date} last_updated Last request time
+   */
   public static createNewState(
     provider: string,
     id: string,
@@ -46,10 +63,28 @@ export class UserState {
     );
   }
 
+  /**
+   * Get a user's state
+   *
+   * @param {string} provider Provider information
+   * @param {string} id User's ID
+   * @return {State | undefined} User's state or `undefined`
+   * if there's no saved state
+   */
   public static getState(provider: string, id: string): State | undefined {
     return UserState.userState.get(`${provider}@${id}`);
   }
 
+  /**
+   * Update a user's state
+   *
+   * @param {string} provider Provider information
+   * @param {string} id User's ID
+   * @param {string} service Service identifier
+   * @param {number} state Service state
+   * @param {string} text Accumulated text
+   * @param {Date} last_updated Last request time
+   */
   public static modifyState(
     provider: string,
     id: string,
@@ -69,6 +104,11 @@ export class UserState {
     );
   }
 
+  /**
+   * Delete a user's state
+   * @param {string} provider Provider information
+   * @param {string} id User's ID
+   */
   public static deleteState(
     provider: string,
     id: string
